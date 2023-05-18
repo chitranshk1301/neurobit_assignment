6d73db16a0bfbf53456f04edd80e0bd1a098e170
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2023-05-18 16:03:29 +0530

    header completed

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import AccountCircleTwoToneIcon from '@mui/icons-material/AccountCircleTwoTone';
+
+const header = () => {
+    return (
+        <div>
+            <div className="p-4 sm:ml-64 h-24 grid grid-cols-2 gap-x-96">
+                <p className="font-dmSans font-bold text-3xl mt-5 ml-10">Test_Study</p>
+                <p className="ml-96 mt-5 font-dmSans font-medium"><AccountCircleTwoToneIcon style={{ marginRight: '10', color: 'cyan' }} />User Name</p>
+            </div>
+            <hr className="mt-3" />
+        </div>
+
+    )
+}
+
+export default header;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const navbar = () => {
     return (
         <aside className='fixed top-0 left-0 z-40 w-64 h-screen transition-transform -translate-x-full sm:translate-x-0 '>
             <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
-                <p className='text-white mx-16 mt-5 font-bold text-xl font-dmSans'>React Test</p>
+                <p className='text-white mx-10 mt-5 font-bold text-3xl font-dmSans'>React Test</p>
                 <hr className='my-8 h-1 bg-gray-200 border-1 border-gray-950 dark:bg-gray-700' />
                 <div className='mt-10 grid grid-cols-1 gap-5'>
                     <Button style={{color: 'white', marginLeft: '10'}}><GridView style={{marginRight: '10', color: 'white'}}/>Dashboard</Button>
