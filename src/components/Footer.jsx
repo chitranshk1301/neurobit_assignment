ca8c02be5411d79d4eb2ab9054c3cf880579bf51
Chitransh Kulshrestha <chitranshkulshrestha1301@gmail.com>
2023-05-29 14:29:58 +0530

    next button

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,13 @@ import { Link } from "react-router-dom";
 const footer = () => {
     return (
         <div className="p-4 sm:ml-64 mt-28 bg-white flex flex-cols justify-evenly static bottom-0">
-            <Button variant="outlined" style={{color: 'gray'}}>Back</Button>
+            <Button variant="outlined" style={{ color: 'gray' }}>Back</Button>
             <Link to="/">
                 <p className="text-sm mt-3">Cancel Montage</p>
             </Link>
-            <Button variant="contained">Next</Button>
+            <Link to="/listChannel">
+                <Button variant="contained">Next</Button>
+            </Link>
         </div>
     )
 }
